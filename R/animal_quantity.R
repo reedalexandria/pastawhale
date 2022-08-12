0a910b062cc1007b25f6af96b37b67641939c948
reedalexandria <reed.alexandria@outlook.com>
2022-08-12 11:36:26 -0700

    Initial commit

---
+++ R/animal_quantity.R
@@ -0,0 +1,13 @@
+#' Calculates the quantity of marine animals
+#'
+#' @param whales number of whales
+#' @param turtles number of turtles
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' animal_quantity(whale = 30, turtle = 22)
+animal_quantity <- function(whales, turtles) {
+  whales + turtles
+}
